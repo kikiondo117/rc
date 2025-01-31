111ed60269b933c14e11349738dbdcf340031c57
Carlos Vera <kikiondo117@gmail.com>
2025-01-31 16:27:22 -0600

    About us

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,9 +1,9 @@
 export default function About() {
   return (
     <div className="mx-4 flex flex-col gap-4">
-      <h2 className="text-2xl w-fit font-medium">Sobre nosotros</h2>
+      <h2 className="text-2xl  font-medium text-center">¡Sobre nosotros!</h2>
 
-      <div>
+      <div className="font-light">
         En Radio Chilanga, nuestra misión es conectar con oyentes dando
         contenido que aporte valor sin importar el lugar desde nos sintonicen,
         de igual manera queremos ofrecer una plataforma donde locutores de todas
@@ -12,7 +12,11 @@ export default function About() {
       </div>
 
       <div className="flex flex-col justify-center items-center">
-        <img src="https://i.imgur.com/0f0u4oG.png" alt="Manue Vera" />
+        <img
+          className="w-48 h-48"
+          src="https://res.cloudinary.com/kikis-javascript/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1738362754/rc/locutores/feliciano_wifbtq.jpg"
+          alt="Manue Vera"
+        />
         <div className="flex gap-2">
           <p>Manuel Vera - Director</p>
         </div>
